46a6d8b3a5c0f1eb58c97525460ede089f17ee69
Chinmay <chinmaysharma45@gmail.com>
2020-03-27 15:37:40 +0530

    feat: satdata table v1.0

--- app/routes.py
+++ app/routes.py
@@ -5,21 +5,42 @@ from flask import jsonify
 
 @app.route('/satdata/top100/json')
 def top100json():
-    return jsonify({'data':satellitesData.saTlist})
+    return jsonify({'data':satellitesData.satList})
 
-@app.route('/satdata/top100/text')
-def top100text():
+@app.route('/satdata/top100/table')
+def top100table():
     data = ''
-    for sat in satellitesData.saTlist:
-        data += str(sat)+'<br><br>'
+    for sat in satellitesData.satList:
+        data += '''<tr>
+                        <td> ''' + str(sat['name']) + '''</td>
+                        <td> ''' + str(sat['catalog_number']) + '''</td>
+                        <td> ''' + str(sat['classification']) + '''</td>
+                        <td> ''' + str(sat['launch_year']) + '''</td>
+                        <td> ''' + str(sat['launch_number_of_the_year']) + '''</td>
+                        <td> ''' + str(sat['inclination(deg)']) + ''' </td>
+                    </tr>'''
         
     return '''
         <html>
             <head>
-                <title>Home Page - Microblog</title>
+                <title>Home Page - SatTrack</title>
+                <style>
+                    table, td, th {
+                        border: 1px solid black
+                    }
+                </style>
             </head>
             <body>
                 <h1>Satellite Data</h1>
-                <p> ''' + data + ''' </p>
+                <table style="width:100%"> 
+                    <tr>
+                        <th>Name</th>
+                        <th>Catalog Number</th>
+                        <th>Classification</th>
+                        <th>Launch Year</th>
+                        <th>Launch Num of the Year</th>
+                        <th>Inclination (deg)</th>
+                    </tr>
+                ''' + data + ''' </table>
             </body>
         </html>'''
--- app/service/satellitesData.py
+++ app/service/satellitesData.py
@@ -1,10 +1,10 @@
 from ..models.Satellite import Satellite
 
 sat = Satellite('x',5,8,4,8,4,2,5,2)
-sat2 = Satellite('y',5,8,4,8,4,2,5,2)
-sat3 = Satellite('z',5,8,4,8,4,2,5,2)
+sat2 = Satellite('y',6,8,4,8,4,2,5,2)
+sat3 = Satellite('z',7,8,4,8,4,2,5,2)
 
-saTlist = []
-saTlist.append(sat.getSatDataText())
-saTlist.append(sat2.getSatDataText())
-saTlist.append(sat3.getSatDataText())
+satList = []
+satList.append(sat.getSatData())
+satList.append(sat2.getSatData())
+satList.append(sat3.getSatData())
