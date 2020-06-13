e1a04926ba1f009164dfc2daa4133cadba4ec249
Chinmay <chinmaysharma45@gmail.com>
2020-06-13 17:00:23 +0530

    fix: array long lat for orbits

--- app/service/issData.py
+++ app/service/issData.py
@@ -38,7 +38,7 @@ def getOrbits():
     coordinateArr = []
 
     for i in range(len(subsatLat)):
-        coordinateArr.append([subsatLat[i],subsatLong[i]])
+        coordinateArr.append([subsatLong[i],subsatLat[i]])
     
     # print(coordinateArr)
 
