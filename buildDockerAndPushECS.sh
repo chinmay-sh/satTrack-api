46231ee0460ef0599d5173f81140caedb47dcd2f
Aditya Karnam <akarnam37@gmail.com>
2020-07-06 11:46:36 +0530

    Update buildDockerAndPushECS.sh

--- buildDockerAndPushECS.sh
+++ buildDockerAndPushECS.sh
@@ -5,7 +5,7 @@ if [ -z "$versionTag"]; then
   exit
 fi
 echo "Building SatTrack API for version: " $versionTag
-# docker build https://github.com/the-redlord/satTrack-api.git#$versionTag --tag sattrackapi:$versionTag
+docker build https://github.com/the-redlord/satTrack-api.git#$versionTag --tag sattrackapi:$versionTag
 echo "Done building satTrack:$versionTag"
 echo "Uploading to ECR"
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 605517810339.dkr.ecr.ap-south-1.amazonaws.com
